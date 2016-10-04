b72d0c4707514a5a72bc61c8b3416fcec1dbd6ac
Daniel Lopez <Daniel Lopez>
2016-10-04 18:18:07 -0400

    update

---
+++ Fit/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('tittle')
+    Laravel
+@stop
+
+@section('content')
+    <div class="content">
+        <div class="title m-b-md">
+                    Laravel
+    </div>
+@stop    
